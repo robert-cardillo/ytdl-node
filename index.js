fb9834db9f6c3836766a6ca94752b4877470f09e
robert-cardillo <1337570+robert-cardillo@users.noreply.github.com>
2024-05-25 18:14:06 +0300

    download interactive

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 import fs from 'fs'
 import ytdl from 'ytdl-core'
+import readline from 'readline'
 
-const id = process.argv[2]
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
 
-if (!id) {
-  console.log('Usage: node index.js <video-id>')
-  process.exit(1)
+const askForVideoUrl = () => {
+  rl.question('Enter the video URL: ', async (videoUrl) => {
+    await downloadByVideoUrl(videoUrl)
+    askForVideoUrl()
+  })
 }
 
-const info = await ytdl.getInfo(id)
-const videos = info.formats.filter((format) => format.hasVideo)
-const itag18 = videos.find((video) => video.itag === 18)
-const url = itag18.url
-const filename = info.videoDetails.title + '.mp4'
-console.log('Downloading', filename)
-const response = await fetch(url)
-const buffer = await response.arrayBuffer()
-fs.writeFileSync(filename, Buffer.from(buffer))
+askForVideoUrl();
+
+const downloadByVideoUrl = async (videoUrl) => {
+  const downloadsFolder = './downloads'
+  if (!fs.existsSync(downloadsFolder)) {
+    fs.mkdirSync('./downloads')
+  }
+  const id = ytdl.getURLVideoID(videoUrl)
+  const info = await ytdl.getInfo(id)
+  const videos = info.formats.filter((format) => format.hasVideo)
+  const itag18 = videos.find((video) => video.itag === 18)
+  const url = itag18.url
+  const filename = info.videoDetails.title + '.mp4'
+  console.log('Downloading', filename)
+  const response = await fetch(url)
+  const buffer = await response.arrayBuffer()
+  fs.writeFileSync(`${downloadsFolder}/${filename}`, Buffer.from(buffer))
+}
